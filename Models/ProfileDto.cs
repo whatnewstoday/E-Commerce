98c1752f2ebb038548d46415ab79dce621d0a442
whatnewstoday <ninhlegiabao2004@gmail.com>
2025-05-23 17:04:28 +0700

    Add files via upload

---
+++ Models/ProfileDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BestStoreMVC.Models
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "The first name field is required"), MaxLength(100)]
+        public string FirstName { get; set; } = "";
+        [Required(ErrorMessage = "The last name field is required"), MaxLength(100)]
+        public string LastName { get; set; } = "";
+        [Required, EmailAddress MaxLength(100)]
+        public string Email { get; set; } = ""; 
+        [Phone(ErrorMessage = "The first name field is required"), MaxLength(100)]
+        public string FirstName { get; set; } = "";
+    }
+}
